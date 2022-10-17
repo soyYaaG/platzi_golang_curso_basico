1370995e1447ca7708c1df03c2dc11a50837c822
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2022-10-16 19:13:56 -0500

    ejemplo operadores aritméticos

--- src/main.go
+++ src/main.go
@@ -3,35 +3,40 @@ package main
 import "fmt"
 
 func main() {
-	// Declaración de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.1415
-
-	fmt.Println("pi:", pi)
-	fmt.Println("pi2:", pi2)
-
-	// Declaración de variables enteras
-	base := 12
-	var altura int = 14
-	var area int
-
-	fmt.Println("base:", base)
-	fmt.Println("altura:", altura)
-	fmt.Println("area:", area)
-
-	// Zero values
-	var a int
-	var b float64
-	var c string
-	var d bool
-
-	fmt.Println("a:", a)
-	fmt.Println("b:", b)
-	fmt.Println("c:", c)
-	fmt.Println("d:", d)
 
 	// Area cuadrado
 	const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado
 	fmt.Println("Area cuadrado:", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	// Suma
+	result := x + y
+	fmt.Println("Suma:", result)
+
+	// Resta
+	result = y - x
+	fmt.Println("Resta:", result)
+
+	// Multiplicación
+	result = x * y
+	fmt.Println("Multiplicación:", result)
+
+	// División
+	result = y / x
+	fmt.Println("División:", result)
+
+	// Modulo
+	result = y % x
+	fmt.Println("Modulo:", result)
+
+	// Incremental
+	x++
+	fmt.Println("Incremental:", x)
+
+	// Decremental
+	x--
+	fmt.Println("Decremental:", x)
 }
