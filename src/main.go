44f9221c296385b74e6e1253417b6246190c00f0
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2022-10-16 20:08:48 -0500

    conditions example

--- src/main.go
+++ src/main.go
@@ -1,36 +1,82 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func esPar(value int) bool {
+	if module := value % 2; module == 0 {
+		return true
+	}
+	return false
+}
+
+func getLogin(username, password string) {
+	/* Esta función compara el username y el password y verificar si coincide
+	hacer login de esta manera (username y password en el código), es una muy mala práctica
+	usualmente los frameworks utilizan librerías para hacer este check
+	con passwords encriptados desde la base de datos.
+	*/
+	if username == "soyyaag" && password == "soyyaag" {
+		fmt.Println("You are logged")
+	} else {
+		fmt.Println("Username or password incorrect")
+	}
+}
 
 func main() {
+	valor1 := 1
+	valor2 := 2
 
-	// For condicinal
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	if valor1 == 1 {
+		fmt.Println("Es 1")
+	} else {
+		fmt.Println("No es 1")
 	}
 
-	fmt.Printf("\n")
-
-	// For while
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
+	// With and
+	if valor1 == 1 && valor2 == 3 {
+		fmt.Println("Es verdad")
 	}
 
-	fmt.Printf("\n")
+	// With or
+	if valor1 == 0 || valor2 == 2 {
+		fmt.Println("Es verdad, OR")
+	}
 
-	// For forever
-	// Tener cuidado. Se va a ejecutar infinitamente ya que no tiene ninguna condición para cerrarse.
-	// counterForever := 0
-	// for {
-	// 	fmt.Println(counterForever)
-	// 	counterForever++
-	// }
+	// Convertir texto a número
+	value, err := strconv.Atoi("53")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Value:", value)
 
-	fmt.Printf("\n")
+	// Evaluar número par e impar
+	fmt.Println(esPar(2))
+	fmt.Println(esPar(3))
 
-	for i := 10; i > 0; i-- {
-		fmt.Println(i)
+	// Función para hacer login
+	username := "soyyaag"
+	password := ""
+	getLogin(username, password)
+
+	switch modulo := 4 % 2; modulo {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
+	}
+
+	// Sin condición
+	valueSwitch := 50
+	switch {
+	case valueSwitch > 100:
+		fmt.Println("Es mayor a 100")
+	case valueSwitch < 0:
+		fmt.Println("Es menor a 0")
+	default:
+		fmt.Println("No condición")
 	}
 }
