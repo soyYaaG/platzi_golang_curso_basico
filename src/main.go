414f18f41b3565ddcf1dd1f3a5efc1b89b17da4c
Yerson Alexander Arredondo García <yerson-302@hotmail.com>
2022-10-16 19:27:21 -0500

    fmt package

--- src/main.go
+++ src/main.go
@@ -3,40 +3,25 @@ package main
 import "fmt"
 
 func main() {
+	// Declaración de variables
+	helloMessage := "Hello"
+	worldMessage := "world"
 
-	// Area cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-	fmt.Println("Area cuadrado:", areaCuadrado)
+	// Println
+	fmt.Println(helloMessage, worldMessage)
+	fmt.Println(helloMessage, worldMessage)
 
-	x := 10
-	y := 50
+	// Printf
+	nombre := "Platzi"
+	cursos := 500
+	fmt.Printf("%s tiene más de %d cursos\n", nombre, cursos)
+	fmt.Printf("%v tiene más de %v cursos\n", nombre, cursos)
 
-	// Suma
-	result := x + y
-	fmt.Println("Suma:", result)
+	// Sprintf
+	message := fmt.Sprintf("%s tiene más de %d cursos", nombre, cursos)
+	fmt.Println(message)
 
-	// Resta
-	result = y - x
-	fmt.Println("Resta:", result)
-
-	// Multiplicación
-	result = x * y
-	fmt.Println("Multiplicación:", result)
-
-	// División
-	result = y / x
-	fmt.Println("División:", result)
-
-	// Modulo
-	result = y % x
-	fmt.Println("Modulo:", result)
-
-	// Incremental
-	x++
-	fmt.Println("Incremental:", x)
-
-	// Decremental
-	x--
-	fmt.Println("Decremental:", x)
+	// Tipo datos
+	fmt.Printf("helloMessage: %T\n", helloMessage)
+	fmt.Printf("cursos: %T\n", cursos)
 }
